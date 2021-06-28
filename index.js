1c8ce2fe601699bf9846ba1e26f40741b04cd397
Rosa <rosarenee@att.net>
2021-06-28 15:55:34 -0500

    commit tree 894d503f5e58b7e2b9a8e76f8e818b2a0d151a87

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+
+  if (feet <= 400){
+    return "This one is on me!"
+  }
+  else if (feet  > 2000 && feet <= 2500 ){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else ( feet <= 2500)
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(order){
   // Write your code here!
+  switch (order) {
+    case 'generous':
+      return  'Thank you so much.';
+      break;
+        
+    case 'not as generous':
+      return  'Thank you.';
+
+    case 'thanks for everything':
+      return  'Bye.';
+  }
+
 }
